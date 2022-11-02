c7fe722094efcaff22ae024dd5532541b05e7c59
Johanna Eklundh <jojo.eklundh@gmail.com>
2022-11-02 13:56:20 +0100

    Knapp kan skapa kapsel

--- timecapsule/src/App.js
+++ timecapsule/src/App.js
@@ -1,13 +1,23 @@
+import React from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [capsules, setCapsules] = useState([]);
+  const createCapsule = () => {
+    const newCapsule = "test"; //skapar ny kapsel med namnet test
+    setCapsules([...capsules, newCapsule]); // lägger till den nya kaspeln i en array med alla andra
+  };
+
   return (
     <div>
       <h1> TIME CAPSULE </h1>
-      <button type="button">Create new capsule</button>
-      <div className="capsuleContainer">
-        <div className="capsule">Summer 2022</div>
-      </div>
+      <button type="button" onClick={createCapsule}>
+        Create new capsule
+      </button>
+      {capsules.map((capsule, index) => {
+        return <p className="capsule"> {capsule} </p>;
+      })}
     </div>
   );
 }
