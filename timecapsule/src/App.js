1d9778b29a7c8f6aa0d29c3a4de2194a8860f609
johannaeklundh <114442121+johannaeklundh@users.noreply.github.com>
2022-11-15 10:38:33 +0100

    New Component CreateNewCapsule

--- timecapsule/src/App.js
+++ timecapsule/src/App.js
@@ -1,71 +1,31 @@
 import React from "react";
 import "./App.css";
 import { useState } from "react";
-import Popup from "react-animated-popup";
+
 import Capsule from "./components/Capsule";
-import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import CapsulePlus from "./CapsulePlusRemovebg.png"; // with import
 import OpenedCapsule from "./openedCapsule.png";
+import CreateNewCapsule from "./components/CreateNewCapsule";
+import Popup from "react-animated-popup";
 
 function App() {
   const [capsules, setCapsules] = useState([]);
   const [visible, setVisible] = useState(false);
-  const [tempCap, setTempCap] = useState("");
-  const [startDate, setStartDate] = useState(new Date());
-  const [endDate, setEndDate] = useState(new Date());
 
-  const createCapsule = () => {
-    if (tempCap === "") return;
-    const days =
-      (endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60 * 24);
-    if (days <= 0) return;
-    const newCapsule = {
-      name: tempCap,
-      day: days,
-    }; //skapar ny kapsel med namnet test
+  const addCapsule = (newCapsule) => {
+    console.log(newCapsule);
     setCapsules([...capsules, newCapsule]); // lägger till den nya kaspeln i en array med alla andra
     setVisible(false);
   };
-
-  const capsuleNameHandler = (event) => {
-    console.log(event.target.value);
-    setTempCap(event.target.value);
-  };
-
   return (
     <div>
       <Popup visible={visible} onClose={() => setVisible(false)}>
-        <div className="popup">
-          <form>
-            <label htmlFor="Memoryname">Name Memory:</label>
-            <input
-              type="text"
-              id="Memoryname"
-              name="Memoryname"
-              onChange={capsuleNameHandler}
-            ></input>
-            <p>Select startdate:</p>
-            <DatePicker
-              selected={startDate}
-              onChange={(date) => setStartDate(date)}
-            />
-            <p>Select enddate:</p>
-            <DatePicker
-              selected={endDate}
-              onChange={(date) => setEndDate(date)}
-            />
-            <br></br>
-          </form>
-          <button type="button" onClick={() => setVisible(false)}>
-            Cancel
-          </button>
-          <button type="button" onClick={createCapsule}>
-            Done
-          </button>
-        </div>
+        <CreateNewCapsule
+          onCapsule={addCapsule}
+          onCanceled={() => setVisible(false)}
+        />
       </Popup>
-
       <div className="top">
         <button id="plus" type="button" onClick={() => setVisible(true)}>
           Create new capsule
