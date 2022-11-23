6eae0944d2c79c532496203ea0bdc37d320a4e0e
johannaeklundh <114442121+johannaeklundh@users.noreply.github.com>
2022-11-23 14:30:07 +0100

    SlideShow funkar

--- src/pages/AvailableCapsule.js
+++ src/pages/AvailableCapsule.js
@@ -1,10 +1,12 @@
 import App from "../App";
 import React from "react";
+import SlideShow from "../components/SlideShow";
 
 const AvailableCapsule = (props) => {
   return (
     <div className="Component">
       <h1>Hej Johanna</h1>
+      <SlideShow></SlideShow>
     </div>
   );
 };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,10 +9,12 @@ import OpenedCapsule from "../openedCapsule.png";
 import CreateNewCapsule from "../components//CreateNewCapsule";
 import Popup from "react-animated-popup";
 import ClosedCapsule from "./AvailableCapsule";
+import { useNavigate } from "react-router-dom";
 
 function Home() {
   const [capsules, setCapsules] = useState([]);
   const [visible, setVisible] = useState(false);
+  const navigate = useNavigate();
 
   const addCapsule = (newCapsule) => {
     console.log(newCapsule);
@@ -39,7 +41,12 @@ function Home() {
         })}
       </div>
       <div className="bottom">
-        <button id="open" type="button" onClick={() => setVisible(true)}>
+        <button
+          id="open"
+          type="button"
+          onClick={() => navigate("/capsule")}
+          style={{ cursor: "pointer" }}
+        >
           <img src={OpenedCapsule} />
         </button>
       </div>
