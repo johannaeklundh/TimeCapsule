acbe51b52961f2c99795019572247faa5c25bf8a
astrandandrea <91681425+astrandandrea@users.noreply.github.com>
2022-11-23 14:48:46 +0100

    inget

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Capsule from "../components/Capsule";
 import "react-datepicker/dist/react-datepicker.css";
 import CapsulePlus from "../CapsulePlus.png"; // with import
-import OpenedCapsule from "../openedCapsule.png";
+import OpenedCapsule from "../openedCapsule.png"; // tillgång att se bilderna
 import CreateNewCapsule from "../components//CreateNewCapsule";
 import Popup from "react-animated-popup";
 import ClosedCapsule from "./AvailableCapsule";
@@ -18,6 +18,8 @@ function Home() {
     console.log(newCapsule);
     setCapsules([...capsules, newCapsule]); // lägger till den nya kaspeln i en array med alla andra
     setVisible(false);
+
+
   };
 
   return (
@@ -28,21 +30,25 @@ function Home() {
           onCanceled={() => setVisible(false)}
         />
       </Popup>
+
       <div className="top">
         <button id="plus" type="button" onClick={() => setVisible(true)}>
           <img src={CapsulePlus} />
         </button>
       </div>
+
       <div className="middle">
         {capsules.map((capsuledata, index) => {
           return <Capsule data={capsuledata}></Capsule>;
         })}
       </div>
+
       <div className="bottom">
         <button id="open" type="button" onClick={() => setVisible(true)}>
           <img src={OpenedCapsule} />
         </button>
       </div>
+
     </div>
   );
 }
