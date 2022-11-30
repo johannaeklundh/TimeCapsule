8e02967d9a1eb5844d953abdcd945c1bb9cd2e53
astrandandrea <91681425+astrandandrea@users.noreply.github.com>
2022-11-30 13:52:46 +0100

    closedcapsule

---
+++ timecapsule/src/App.js
@@ -0,0 +1,37 @@
+import Home from "./pages/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import { ClosedCapsule } from "./pages/ClosedCapsule";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/capsule">Capsule</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/capsule" element={<ClosedCapsule />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+
+export default App;
