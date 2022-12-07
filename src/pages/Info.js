064d12bb08f34cf581444921a8d59445c88925d5
johannaeklundh <114442121+johannaeklundh@users.noreply.github.com>
2022-12-07 16:32:44 +0100

    Lite text till infosidan

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,7 +1,15 @@
 const Info = (props) => {
   return (
     <div>
-      <h1>How to use the app</h1>
+      <h1>How the app works: </h1>
+      <p>
+        In this app you can create multiple Time Capsules to save photos in a
+        fun way. When creating a capsule, choose your own name and between which
+        days you want to upload photos. Photos can only be uploaded to the
+        capsule between the start date and end date. When the time is up, photos
+        can no longer be uploaded to the capsule. Photos that have been added
+        can now be viewed! Enjoy making memories that last forever!
+      </p>
     </div>
   );
 };
